030cb37ea0733f38d8978d1249dcb7da4b80a7d1
Jason Long <jason@long.name>
2013-05-04 20:11:42 -0400

    a little more smarts for the RowSolver

--- src/nonograms/RowSolver.java
+++ src/nonograms/RowSolver.java
@@ -52,7 +52,59 @@ public class RowSolver
 			System.out.println();
 			return;
 		}
+
+		solveForward();
+
 		System.out.println();
+	}
+
+	void solveForward()
+	{
+		solveForward(0,0);
+	}
+
+	void solveForward(int hintIdx, int col)
+	{
+		if (hintIdx >= hint.length ||
+			col >= values.length) return;
+
+		int curHint = hint[hintIdx];
+
+		if (values[col] == -1) {
+			solveForward(hintIdx, col+1);
+			return;
+		}
+		else if (values[col] == 1) {
+			// fill in the rest
+			for (int j = 1; j < curHint; j++) {
+				values[col+j] = 1;
+			}
+			if (col+curHint < values.length) {
+				values[col+curHint] = -1;
+			}
+			solveForward(hintIdx+1, col+curHint+1);
+			return;
+		}
+
+		assert values[col] == 0;
+
+		// look ahead to see if any cell has been set
+
+		for (int j = 1; j < curHint; j++) {
+			if (values[col+j] == -1) {
+				// cannot fit here
+				values[col] = -1;
+				return;
+			}
+		}
+
+		if (col+curHint < values.length) {
+			if (values[col+curHint] == 1) {
+				// this span cannot start here
+				values[col] = -1;
+				return;
+			}
+		}
 	}
 
 	void solveInitial()
